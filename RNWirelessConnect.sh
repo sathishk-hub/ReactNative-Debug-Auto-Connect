dca8c595c1365a9c3b05ffbdce7180cae6fad059
sathishk-hub <sathishkcontact@gmail.com>
2022-11-28 00:52:53 +0530

    update command

--- RNWirelessConnect.sh
+++ RNWirelessConnect.sh
@@ -30,7 +30,7 @@ else
 	sleep 0.1
 	adb shell input keyevent 20 20 20 20 20 20 20 20 20 20 
 	sleep 0.2
-	adb shell input keyevent 19 19 19	
+	adb shell input keyevent 19 19
 	sleep 0.2
 	adb shell input keyevent 66
 	sleep 0.1
